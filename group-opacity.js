565cfa7d83912ea15582ea421eed1a03cd82a475
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-06-18 12:50:58 -0400

    add optional "base-opacity" prop to group-opacity children

--- group-opacity.js
+++ group-opacity.js
@@ -3,12 +3,20 @@ AFRAME.registerComponent('group-opacity', {
     type: 'number',
     default: 1.0
   },
-  update: function () {
-    var opacity = this.data;
-    var children = [].slice.call(this.el.children);
+  update: function() {
+    const opacity = this.data;
+    const children = [].slice.call(this.el.children);
+    const baseOpacities = children.map((child, index) => {
+      const value = child.getAttribute('base-opacity');
+      if (value != null) {
+        return Number(value);
+      } else {
+        return 1;
+      }
+    });
     children
-      .forEach(function (child) {
-        child.setAttribute('opacity', opacity);
+      .forEach((child, index) => {
+        child.setAttribute('opacity', opacity * baseOpacities[index]);
       });
   }
 });
