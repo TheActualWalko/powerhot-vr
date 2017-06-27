e0a82798596815df4fb7163c85a6d83040c0bd24
Andrew Vickory <AndrewVickory@iheartmedia.com>
2017-06-27 12:04:14 -0400

    initial facebook posting implementation.  Photo not publishing

--- shootTheScreen.js
+++ shootTheScreen.js
@@ -1,16 +1,22 @@
-function screenToJpegBlob (sceneId, use360, quality=1) { //args: string, bool, number: 0 < x <= 1
+function screenToJpegBlob (sceneId, use360, quality=0.92) { //args: string, bool, number: 0 < x <= 1
   return new Promise(function (resolve) {
     const screenshot = document.querySelector(`#${sceneId}`).components.screenshot;
     const {camera, size, projection} = screenshot.setCapture(use360 ? 'equirectangular' : 'perspective');
     screenshot.renderCapture(camera, size, projection);
-    screenshot.canvas.toBlob(resolve, 'image/jpeg', quality)
+    screenshot.canvas.toBlob(resolve, 'image/jpeg', quality);
   });
 }
 
+function screenToJpegDataUrl (sceneId, use360, quality=0.92) {
+  const screenshot = document.querySelector(`#${sceneId}`).components.screenshot;
+  const {camera, size, projection} = screenshot.setCapture(use360 ? 'equirectangular' : 'perspective');
+  screenshot.renderCapture(camera, size, projection);
+  return screenshot.canvas.toDataURL()('image/jpeg', quality);
+}
+
 function renderJpegToImage (jpeg) {
-  var newImg = document.createElement('img'),
+  const newImg = document.createElement('img'),
   url = URL.createObjectURL(jpeg);
-  console.log({jpeg})
 
   newImg.onload = function() {
     // no longer need to read the blob so it's revoked
@@ -21,6 +27,21 @@ function renderJpegToImage (jpeg) {
   document.body.appendChild(newImg);
 }
 
-function getJpegAndRender () {
-  return screenToJpeg('armsRace', false).then(renderJpegToImage);
+function sendBlobToFacebook () {
+
 }
+
+function getJpegAndRender () {
+  return screenToJpegBlob('armsRace', false).then(renderJpegToImage);
+}
+
+function getJpegAndSend (message) {
+  return screenToJpegBlob('armsRace', false)
+    .then((blob) => {
+      return ensureLogin()
+      .then(shareImage(message, blob))
+      .then((response) => {console.log({response})})
+      .catch((err) => {console.log({err})})
+    })
+}
+
