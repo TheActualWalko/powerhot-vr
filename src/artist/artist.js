c22fa80b472c1cec885ca3e13e1c4dafc5d6a422
Andrew Vickory <AndrewVickory@iheartmedia.com>
2017-06-28 16:27:30 -0400

    clean up

--- src/artist/artist.js
+++ src/artist/artist.js
@@ -5,13 +5,13 @@ const paddingAdjustment= '-0.4px'
 const imageContainer = `width:${containerWidth};height:${imageHeight};position:absolute;left:${paddingAdjustment};top:${paddingAdjustment};`
 const imageMask = `width:${containerWidth};height:${imageHeight};position:absolute;left:${paddingAdjustment};top:${paddingAdjustment};`
 const imageStyle = `width:${containerWidth};height:${imageHeight};position:absolute;left:${paddingAdjustment};top:${paddingAdjustment};`
-const containerStyle = `position:absolute;left:${paddingAdjustment};top:${imageHeight};padding-top:5px;padding-bottom:5px;background-color:#111;`
+const textContainerStyle = `position:absolute;left:${paddingAdjustment};top:${imageHeight};padding-top:5px;padding-bottom:5px;background-color:#111;`
 const artistNameStyle = `text-align:center;width:${containerWidth};font-weight:500;height:28px;font-size:28px;`
 const h2Style = `text-align:center;width:${containerWidth};font-weight:400;height:20px;font-size:28px;`
 const h3Style = `text-align:center;width:${containerWidth};color:#777;font-weight:300;height:16px;font-size:24px;`
 const ruleStyle = `display:inline-block;text-align:center;width:350px;color:#777;height: 1px;margin-bottom:0;`
 const ruleContainerStyle = `text-align:center;width:${containerWidth};height: 1px;margin-bottom:0;`
-console.log(imageMask.split(';'))
+
 function setArtistProfile({
     image,
     name,
@@ -35,7 +35,8 @@ function setArtistProfile({
         <rect x="0" y="0" width="396" height="132" fill="url(#gradient)"/>
       </svg>
     </div>
-    <div style="${containerStyle}" >
+
+    <div style="${textContainerStyle}" >
       <h1 style=${artistNameStyle}>${name}</h1>
       <div style=${ruleContainerStyle}><hr style="${ruleStyle}" /><div>
       <h2 style="${h2Style}">Currently Perfoming:</h2>
@@ -48,5 +49,5 @@ function setArtistProfile({
 }
 
 setTimeout(() => {
-  setArtistProfile(window.BrunoMars);
+  setArtistProfile(window.artists.BrunoMars);
 }, 0)
